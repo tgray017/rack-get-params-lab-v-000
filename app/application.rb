2a1e43868dac4dc173ef03f0f59f54a6d41650c2
tgray017 <tgray017@gmail.com>
2019-02-26 14:03:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -28,7 +28,7 @@ class Application
         @@cart << item
         resp.write "added #{item}"
       else
-        resp.write "Item not found"
+        resp.write "We don't have that item"
       end
     else
       resp.write "Path Not Found"
